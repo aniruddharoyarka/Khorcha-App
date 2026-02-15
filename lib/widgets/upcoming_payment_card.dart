cda2f108eabc52576ea37abfbf765318ba19807f
Aniruddha Roy Arka <aniruddharoyarka2@gmail.com>
2026-02-16 02:30:37 +0600

    removed padding from upcoming payment card

--- lib/widgets/upcoming_payment_card.dart
+++ lib/widgets/upcoming_payment_card.dart
@@ -8,47 +8,44 @@ class UpcomingPaymentCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 20),
-        child: GestureDetector(
-          onTap: () => _showTransactionDetails(context, payment),
-            child: Container(
-              width: 180,
-              margin: const EdgeInsets.only(right: 15),
-              padding: const EdgeInsets.all(15),
-              decoration: BoxDecoration(
-                color: Color(0xFF03624C), // Use a theme color
-                borderRadius: BorderRadius.circular(20),
+    return GestureDetector(
+      onTap: () => _showTransactionDetails(context, payment),
+        child: Container(
+          width: 180,
+          margin: const EdgeInsets.only(right: 15),
+          padding: const EdgeInsets.all(15),
+          decoration: BoxDecoration(
+            color: Color(0xFF03624C), // Use a theme color
+            borderRadius: BorderRadius.circular(20),
+          ),
+          child: Row(
+            children: [
+              const CircleAvatar(
+                radius: 22,
+                backgroundColor: Colors.white,
+                child: Icon(Icons.calendar_month, color: Color(0xFF03624C), size: 25),
               ),
-              child: Row(
-                children: [
-                  const CircleAvatar(
-                    radius: 22,
-                    backgroundColor: Colors.white,
-                    child: Icon(Icons.calendar_month, color: Color(0xFF03624C), size: 25),
-                  ),
-                  const SizedBox(width: 12),
-                  Expanded(
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Text(
-                          payment.title,
-                          style: const TextStyle(color: Colors.white70, fontSize: 14),
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        Text(
-                          "৳${payment.amount.toStringAsFixed(0)}",
-                          style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
-                        ),
-                      ],
+              const SizedBox(width: 12),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      payment.title,
+                      style: const TextStyle(color: Colors.white70, fontSize: 14),
+                      overflow: TextOverflow.ellipsis,
                     ),
-                  ),
-                ],
+                    Text(
+                      "৳${payment.amount.toStringAsFixed(0)}",
+                      style: const TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 18),
+                    ),
+                  ],
+                ),
               ),
-            )
-        ),
+            ],
+          ),
+        )
     );
   }
 }
